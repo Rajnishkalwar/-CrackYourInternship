38342772d1260ba5d8820dd6bbf455edc0cf5bac
Rajnish Prasad Kalwar <98279952+Rajnishkalwar@users.noreply.github.com>
2024-10-07 08:52:21 +0530

    Time: 3 ms (59.09%), Space: 9 MB (67.55%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -18,7 +18,7 @@ public:
         while(i<s.length()){
             if(s[i]>='0' && s[i]<='9'){
                 ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1){
+                if(-1*ans<INT_MIN && sign==-1){
                     return INT_MIN;
                 }
                 else if(ans>INT_MAX && sign==1){
